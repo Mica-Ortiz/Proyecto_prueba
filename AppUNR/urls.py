0539b4cb9657fa3e4b02c1ff424b9a2ddf7fd233
Micaela Ortiz <ortiz.micaela.ailen@gmail.com>
2023-06-08 21:05:10 -0300

    Segundo commit, ya tenemos el index y la base

---
+++ AppUNR/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from AppUNR import views
+
+urlpatterns = [
+    path('', views.inicio, name="Inicio"),
+    path('profesores/', views.profesores, name="Profesores"),
+    path('estudiantes/', views.estudiantes, name="Estudiantes"),
+    path('cursos/', views.cursos, name="Cursos"),
+    path('entregables/', views.entregables, name="Entregables")
+]
--- AppUNR/views.py
+++ AppUNR/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio(request):
+    return render(request, "AppUNR/index.html")
+
+def cursos(request):
+    return render(request, "AppUNR/cursos.html")
+
+def profesores(request):
+    return render(request, "AppUNR/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppUNR/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppUNR/entregables.html")
